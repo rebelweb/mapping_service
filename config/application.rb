9e7fb0894cbff9dbae6fc0d3b7384487a2434b30
Ryan Condron <rebelwebdevelopment@gmail.com>
2022-10-02 09:06:12 -0500

    spec for version number

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'bundler/setup'
+Bundler.require
+
 Dir.glob('lib/**/*.rb').each do |f|
   require "./#{f}"
 end
---
+++ spec/mapping_service_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe MappingService do
+  it 'has a version number' do
+    version = MappingService::VERSION
+    version_parts = version.split('.')
+
+    expect(version_parts[0]).to eq('0')
+    expect(version_parts[1]).to eq('0')
+  end
+end
