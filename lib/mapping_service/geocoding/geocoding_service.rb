dc071fe9743a860313679c67b5b75965b62dd6f9
Ryan Condron <rebelwebdevelopment@gmail.com>
2022-11-02 00:03:39 -0500

    Implement ProviderResponseRepository in GeocodeService

--- lib/mapping_service/geocoding/geocoding_service.rb
+++ lib/mapping_service/geocoding/geocoding_service.rb
@@ -5,26 +5,27 @@ module MappingService
     class GeocodingService
       attr_reader :geocode_retriever
 
-      def initialize(geocode_retriever: GeocodeRetriever.new)
+      def initialize(geocode_retriever: GeocodeRetriever.new,
+        repository: ProviderResponseRepository.new)
         self.geocode_retriever = geocode_retriever
+        self.provider_response_repository = repository
       end
 
       def call(query:)
-        cache = ProviderResponse.where(
-          name: 'Here',
-          query: query
-        ).last
+        cache = provider_response_repository.retrieve(
+          query: query,
+          provider: 'Here'
+        )
 
         response = cache
 
         unless cache
           response = geocode_retriever.call(query: query)
 
-          ProviderResponse.create(
+          provider_response_repository.create(
             query: query,
             provider: 'Here',
             response: response,
-            created_at: Time.zone.now
           )
         end
 
@@ -33,7 +34,8 @@ module MappingService
 
       private
 
-      attr_writer :geocode_retriever
+      attr_reader :provider_response_repository
+      attr_writer :geocode_retriever, :provider_response_repository
     end
   end
 end
