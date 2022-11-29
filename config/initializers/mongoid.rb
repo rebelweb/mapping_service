4a8b79c9e903b4044cecb0ee7461df90c8233ed1
Ryan Condron <rebelwebdevelopment@gmail.com>
2022-11-29 10:00:07 -0600

    update database config for CI/CD pipeline

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -2,4 +2,32 @@
 
 require 'mongoid'
 
-Mongoid.load!('./config/database.yml', ENV.fetch('APP_ENV', 'development'))
+class DatabaseConfig
+  class << self
+    def database_name
+      case ENV.fetch('APP_ENV', 'development')
+      when 'development'
+        'mapping_service_development'
+      when 'test'
+        'mapping_service_test'
+      when 'production'
+        ENV.fetch('DATABASE_NAME', 'mapping_service_production')
+      else
+        ENV.fetch('DATABASE_NAME')
+      end
+    end
+
+    def hosts
+      ENV.fetch('DATABASE_HOST', 'localhost:27017').split(',')
+    end
+  end
+end
+
+Mongoid.configure do |config|
+  config.clients.default = {
+    hosts: DatabaseConfig.hosts,
+    database: DatabaseConfig.database_name
+  }
+
+  config.log_level = :warn
+end
