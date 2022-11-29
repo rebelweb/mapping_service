c3a0d0a23772f303bd1827d96cf6cbcbc975c78e
Ryan Condron <rebelwebdevelopment@gmail.com>
2022-11-29 10:39:16 -0600

    try to fix vcr in github actions

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -9,6 +9,6 @@ VCR.configure do |vcr|
   vcr.cassette_library_dir = 'spec/fixtures/cassettes'
   vcr.hook_into :webmock
   vcr.allow_http_connections_when_no_cassette = true
-  vcr.filter_sensitive_data('<API_KEY>') { ENV.fetch('HERE_API_KEY') }
-  vcr.filter_sensitive_data('<GOOGLE_API_KEY>') { ENV.fetch('GOOGLE_API_KEY') }
+  vcr.filter_sensitive_data('<API_KEY>') { ENV.fetch('HERE_API_KEY', nil) }
+  vcr.filter_sensitive_data('<GOOGLE_API_KEY>') { ENV.fetch('GOOGLE_API_KEY', nil) }
 end
