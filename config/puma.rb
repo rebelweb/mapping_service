b4086615423c0537d72b2bcbb5263890afc8b396
Ryan Condron <rebelwebdevelopment@gmail.com>
2023-03-21 23:00:06 -0500

    Add Configuration Options for puma (#27)
    * add environment variables to configure puma
    
    * add puma binstubs
    
    * exclude binstubs from rubocop

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
-workers 3
-threads 2, 5
+min_threads = ENV.fetch('PUMA_MIN_THREADS', 2)
+max_threads = ENV.fetch('PUMA_MAX_THREADS', 5)
+
+workers ENV.fetch('PUMA_WORKERS', 3)
+threads min_threads, max_threads
 
 preload_app!
 
