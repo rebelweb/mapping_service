87b25237ddf35166b254b60ad337dd1eeac48465
Ryan Condron <rebelwebdevelopment@gmail.com>
2023-04-11 22:17:20 -0500

    add expired? to api_key model

--- lib/mapping_service/auth/api_key.rb
+++ lib/mapping_service/auth/api_key.rb
@@ -12,6 +12,12 @@ module MappingService
       field :geocoding, type: Boolean
       field :expires_at, type: DateTime
       field :created_at, type: DateTime
+
+      def expired?
+        return false if expires_at.nil?
+
+        expires_at < Time.current
+      end
     end
   end
 end
--- spec/auth/api_key_spec.rb
+++ spec/auth/api_key_spec.rb
@@ -5,21 +5,31 @@ require 'spec_helper'
 module MappingService
   module Auth
     RSpec.describe ApiKey do
-      let(:key) { SecureRandom.hex(32) }
-      let(:expires_at) { }
-
-      it 'creates a key' do
-        api_key = described_class.create(
-          key: key,
+      let(:api_key) do
+        ApiKey.new(
+          key: SecureRandom.hex(32),
           description: 'Sample',
-          active: true,
           geocoding: true,
           admin: true,
-          expires_at: Time.zone.now + 30.days,
+          expires_at: Time.zone.now + 90.days,
           created_at: Time.zone.now
         )
+      end
 
-        expect(api_key).to be_truthy
+      describe '#expired?' do
+        it 'returns true if the expired date is before to current time' do
+          api_key.expires_at = Time.current - 1.day
+          expect(api_key.expired?).to be_truthy
+        end
+
+        it 'returns false if the expires date is after the current time' do
+          expect(api_key.expired?).to be_falsey
+        end
+
+        it "returns false if the key doesn't expire" do
+          api_key.expires_at = nil
+          expect(api_key.expired?).to be_falsey
+        end
       end
     end
   end
